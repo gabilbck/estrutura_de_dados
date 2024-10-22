ea20a7e139138d4e34b0a7bb9f35c97ec86b1381
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-10-22 20:39:26 -0300

    add

---
+++ aulas/aula_lista/exe3.c
@@ -0,0 +1,5 @@
+/*
+Construa um algoritmo que contenha uma lista estática homogênea para guardar seis números reais, 
+solicite ao usuário os números e apresente para ele a média dos números. 
+*/
+
