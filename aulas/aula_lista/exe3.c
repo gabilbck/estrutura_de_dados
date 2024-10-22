279ec4a610c60292c60cfbd5a90706e952694200
Gabrieli Eduarda Lembeck <79219121+gabilbck@users.noreply.github.com>
2024-10-22 20:52:13 -0300

    add

--- aulas/aula_lista/exe3.c
+++ aulas/aula_lista/exe3.c
@@ -3,3 +3,20 @@ Construa um algoritmo que contenha uma lista estática homogênea para guardar s
 solicite ao usuário os números e apresente para ele a média dos números. 
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+    float lista[6];  // Alterar o tipo para float ou double, dependendo da precisão desejada
+    
+    for(int i=0; i<6; i++){
+        printf("Digite o %iº número: ", i+1);
+        scanf("%f", &lista[i]);  // Alterar o formato de entrada para %f para float
+    }
+    
+    for(int i=0; i<6; i++){
+        printf("%.2f\n", lista[i]);  // Usar %.2f para exibir com 2 casas decimais (ou ajusta conforme necessário)
+    }
+    
+    return 0;
+}
